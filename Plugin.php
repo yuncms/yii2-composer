dd41fe30bb4f74c52beb43fdbdfe227a41156af0
Peter <xutongle@gmail.com>
2017-09-22 19:49:38 +0800

    beta

--- Plugin.php
+++ Plugin.php
@@ -32,19 +32,18 @@ class Plugin implements PluginInterface
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
         $this->_vendorDir = rtrim($composer->getConfig()->get('vendor-dir'), '/');
-        $file = $this->_vendorDir . '/yuncms/modules.php';
-        if (!is_file($file)) {
-            @mkdir(dirname($file), 0777, true);
-            file_put_contents($file, "<?php\n\nreturn [];\n");
-        }
+        $this->mkFile($this->_vendorDir . '/' . Installer::BACKEND_MODULE_FILE);
+        $this->mkFile($this->_vendorDir . '/' . Installer::MODULE_FILE);
+        $this->mkFile($this->_vendorDir . '/' . Installer::MIGRATION_FILE);
+        $this->mkFile($this->_vendorDir . '/' . Installer::TRANSLATE_FILE);
+    }
 
-        $file = $this->_vendorDir . Installer::MIGRATION_FILE;
-        if (!is_file($file)) {
-            @mkdir(dirname($file), 0777, true);
-            file_put_contents($file, "<?php\n\nreturn [];\n");
-        }
-
-        $file = $this->_vendorDir . Installer::TRANSLATE_FILE;
+    /**
+     * 创建文件
+     * @param string $file
+     */
+    public function mkFile($file)
+    {
         if (!is_file($file)) {
             @mkdir(dirname($file), 0777, true);
             file_put_contents($file, "<?php\n\nreturn [];\n");
